647a8f5dcc19e0518d0aae8034b4756c03d70504
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 18:26:29 +0530

    c

---
+++ question6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int employeeID,areacode,hno,pincode;
+    scanf("%d%d%d%d",&hno,&pincode,&employeeID,&areacode);
+    printf("EmployeeID : %d\nArea Code : %d\nHouse Number : %d\nPincode : %d",employeeID,areacode,hno,pincode);
+
+	return 0;
+}
